e7c9d29c899540fa77d29f18df17d2343b619609
jankey <vasili.coder@gmail.com>
2023-04-28 11:41:18 +0300

    factory

---
+++ creational/factory/factory.php
@@ -0,0 +1,27 @@
+<?php
+
+class Worker{
+    private string $name;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+}
+
+class WorkerFactory{
+    public static function make(): Worker
+    {
+        return new Worker();
+    }
+}
+
+$worker = WorkerFactory::make();
+$worker->setName("Some Name");
+var_dump($worker->getName());
