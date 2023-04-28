32950e4d7d710da2ded7a3397f60b01896b8a2ed
jankey <vasili.coder@gmail.com>
2023-04-28 11:17:50 +0300

    singleton

---
+++ creational/singleton/singleton.php
@@ -0,0 +1,42 @@
+<?php
+
+final class Connection{
+
+    private static ?self $instance = null;
+    private static string $name;
+
+    public static function getInstance():self {
+
+        if (self::$instance === null){
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public static function getName(): string
+    {
+        return self::$name;
+    }
+
+    public static function setName(string $name): void
+    {
+        self::$name = $name;
+    }
+
+    public function __clone(): void
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public function __wakeup(): void
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+}
+
+$connection = Connection::getInstance();
+$connection::setName("Laravel");
+
+var_dump($connection::getName());
